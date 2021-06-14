d382a540e758a425ef4a159192f2684d8cacf8b0
BlaiMelendezCatalan <blaimelcat@gmail.com>
2021-06-14 19:10:54 +0200

    Login Activity Added

---
+++ app/src/main/java/com/blaimelcat/serrafit/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.blaimelcat.serrafit.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.blaimelcat.serrafit.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
